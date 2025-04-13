66ef1ad73302785ee0de5e5ea5ed6da911ff9e14
Kozma Kristof <bechryko@gmail.com>
2025-04-13 19:13:38 +0200

    RD-6 Refactor menu styles

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,38 +2,43 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
-import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { GameStartService } from 'src/app/game-start.service';
+import { ControlPanelGroup } from './models';
 import { NewsComponent } from './news';
 
 @Component({
    selector: 'rd-menu',
    templateUrl: './menu.component.html',
    styleUrls: ['./menu.component.scss'],
-   imports: [MatGridList, MatGridTile, NewsComponent, MatButton, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatIcon, MatSuffix],
+   imports: [NewsComponent, MatButton, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatIcon, MatSuffix],
    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
-   private router = inject(Router);
-   private gameStartService = inject(GameStartService);
+   private readonly router = inject(Router);
+   private readonly gameStartService = inject(GameStartService);
 
-   buttonGroup: string = 'menu';
-   setupGameForm = new FormGroup({
+   public readonly ControlPanelGroup = ControlPanelGroup;
+
+   public buttonGroup: ControlPanelGroup = ControlPanelGroup.MAIN_MENU;
+
+   public readonly setupGameForm = new FormGroup({
       seed: new FormControl('', [Validators.pattern('^[0-9a-zA-Z]*$')])
    });
 
-   switchButtonGroup(group: string) {
+   public switchButtonGroup(group: ControlPanelGroup): void {
       this.buttonGroup = group;
       this.setupGameForm.reset();
    }
-   startQuickGame() {
+
+   public startQuickGame(): void {
       this.gameStartService.initStartingParams('');
       this.router.navigateByUrl('/game');
    }
-   startCustomGame() {
+
+   public startCustomGame(): void {
       this.gameStartService.initStartingParams(this.setupGameForm.value.seed ?? '');
       this.router.navigateByUrl('/game');
    }
