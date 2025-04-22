53c36acd3ee956bb782a2f67adc2d17614244510
Kozma Kristof <bechryko@gmail.com>
2025-04-23 00:53:36 +0200

    RD-10 Add custom icon component

---
+++ src/app/shared/components/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+export type IconName = 'info' | 'controller' | 'history' | 'road' | 'guide';
+
+@Component({
+   selector: 'rd-icon',
+   imports: [MatIconModule],
+   templateUrl: './icon.component.html',
+   styleUrl: './icon.component.scss',
+   changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconComponent {
+   public readonly name = input.required<IconName>();
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,3 +1,5 @@
 //@index('./*', (f, _) => `export { ${_.pascalCase(f.name)}Component } from '${f.path}/${f.name}.component';`)
 export { DialogCloseButtonComponent } from './dialog-close-button/dialog-close-button.component';
+export { IconComponent } from './icon/icon.component';
 //@endindex
+export { IconName } from './icon/icon.component';
