4d726b1f66552232fac2bd228b1b060921039474
Kozma Kristof <bechryko@gmail.com>
2025-05-31 15:37:19 +0200

    RD-33 Create music handler for the menu

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,3 +1,4 @@
 //@index('./*', (f, _) => `export { ${_.pascalCase(f.name.split('.')[0])} } from '${f.path}';`)
+export { MusicHandler } from './music-handler';
 export { Song } from './song';
 //@endindex
