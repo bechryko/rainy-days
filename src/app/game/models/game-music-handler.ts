1b729048462a9e9b5c572695504f45d4e048e8c5
Kozma Kristof <bechryko@gmail.com>
2025-06-03 23:13:18 +0200

    RD-34 Add music controller to game

---
+++ src/app/game/models/game-music-handler.ts
@@ -0,0 +1,22 @@
+import { SongPlayContext, SongTag } from '@rainy-days/shared/enums';
+import { MusicHandler, Song } from '@rainy-days/shared/models';
+
+export class GameMusicHandler extends MusicHandler {
+   private static readonly EARLY_GAME_END_SCORE = 750;
+
+   private readonly earlyGameSongs = this.findSongsByTag(SongTag.EARLY_GAME);
+
+   constructor(private readonly getScoreFn: () => number) {
+      super(SongPlayContext.GAME);
+   }
+
+   public override chooseNextSong(previousSong: Song | null): Song {
+      return this.isEarlyGame()
+         ? this.getRandomSong(previousSong, this.earlyGameSongs)
+         : this.getRandomSong(previousSong, this.earlyGameSongs);
+   }
+
+   private isEarlyGame(): boolean {
+      return this.getScoreFn() < GameMusicHandler.EARLY_GAME_END_SCORE;
+   }
+}
